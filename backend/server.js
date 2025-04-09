7ff9ce4e2810cc41b3561608bd0842ed3873e588
Pranav-cse-kalvium <pranav.prakash@kalvium.community>
2025-04-09 09:50:52 +0530

    schema_setup

--- backend/server.js
+++ backend/server.js
@@ -10,6 +10,7 @@ app.use(cors());
 app.use(express.json());
 
 
+// creating routes here 
 const userRoutes = require('./routes/userRoutes');
 const productRoutes = require('./routes/productRoutes');
 const orderRoutes = require('./routes/orderRoutes');

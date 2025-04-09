311e4fd9d00655db7aacebc7124e161667a55fb2
Pranav-cse-kalvium <pranav.prakash@kalvium.community>
2025-04-09 09:41:09 +0530

    Schema creation

---
+++ backend/models/Order.js
@@ -0,0 +1,36 @@
+const mongoose = require('mongoose');
+
+const orderSchema = new mongoose.Schema({
+  buyer: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true,
+  },
+  products: [
+    {
+      product: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Product',
+      },
+      quantity: {
+        type: Number,
+        default: 1,
+      },
+    },
+  ],
+  totalAmount: {
+    type: Number,
+    required: true,
+  },
+  paymentStatus: {
+    type: String,
+    enum: ['pending', 'paid', 'failed'],
+    default: 'pending',
+  },
+  orderedAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = mongoose.model('Order', orderSchema);

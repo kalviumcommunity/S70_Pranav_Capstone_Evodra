894c37733cbb156fab289ce3403f954cba6f4455
Pranav-cse-kalvium <pranav.prakash@kalvium.community>
2025-04-11 14:23:03 +0530

    Added DB read/write

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,9 @@ const express = require('express');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const cors = require('cors');
+const connectDB = require('./config/db');
+connectDB();
+
 
 dotenv.config();
 
@@ -22,7 +25,7 @@ app.use('/api/orders', orderRoutes);
 
 
 mongoose.connect(process.env.MONGO_URI)
-  .then(() => console.log('MongoDB connected'))
+  .then(() => console.log('MongoDB connected successfully'))
   .catch((err) => console.error(err));
 
 const PORT = process.env.PORT || 5000;
